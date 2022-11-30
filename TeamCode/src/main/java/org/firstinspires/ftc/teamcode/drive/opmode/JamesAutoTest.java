b39edbad2c246d9f0e5357ca69ccf1cfcb88d3ae
JamesHaines <102186519+JamesHaines@users.noreply.github.com>
2022-11-30 18:11:10 -0500

    James-
    First auton test, it works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/JamesAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/JamesAutoTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -17,13 +18,18 @@ public class JamesAutoTest extends LinearOpMode{
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         Trajectory straightLine = drive.trajectoryBuilder(new Pose2d())
-                .forward(2)
+                 //.splineTo(new Vector2d(8,8),6)
+                //.splineToSplineHeading(new Pose2d(36,-36,Math.toRadians(90)),Math.toRadians(90))
+                .forward(24)
                 .build();
         waitForStart();
-
+        Trajectory sideways = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(24)
+                .build();
 
         while(opModeIsActive() && !isStopRequested() && ctr == 1){
             drive.followTrajectory(straightLine);
+            drive.followTrajectory(sideways);
             ctr++;
         }
     }
