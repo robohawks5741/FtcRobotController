5c691e3f338f95f3992e4375c37206d0126d7d38
JamesHaines <102186519+JamesHaines@users.noreply.github.com>
2023-01-14 17:30:53 -0500

    Added on tile centering capabilities to the auton prototype.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RightConeDepositorAutonInterface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RightConeDepositorAutonInterface.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
 public interface RightConeDepositorAutonInterface {
     void LeftAndDump();
 
     void RightAndPickup();
 
+    void CenterOnTile(Pose2d position);
+
     void Park(int location);
+
+    //void firstCone();
+
+    void FirstCone();
 }
