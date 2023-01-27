f3344e6fb9b9916f13158af7f72d762e55e9ec21
JamesHaines <102186519+JamesHaines@users.noreply.github.com>
2023-01-27 12:56:04 -0500

    Kv, Ka, KStatic tuned for 13.5v battery. REV flat battery.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -57,9 +57,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.018396055907567793;
-    public static double kA = 0.005;
-    public static double kStatic = 0.024;
+    public static double kV = 0.015706055907567793; //works for battery ~13.5v
+    public static double kA = 0.0035;
+    public static double kStatic = 0.02;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
