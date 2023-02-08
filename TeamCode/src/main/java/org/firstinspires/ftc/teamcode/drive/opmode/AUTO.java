0c77caebf724ce138a1316f5013afd7fda19b91c
JamesHaines <102186519+JamesHaines@users.noreply.github.com>
2023-02-08 11:52:55 -0500

    save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AUTO.java
@@ -24,42 +24,34 @@ import java.util.ArrayList;
 
 @Autonomous
 public class AUTO extends LinearOpMode implements AUTOinterface {
+
+    // Declare all the things...
+
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
-
     SampleMecanumDrive drive = null;
-
     private Servo clawServo;
-
     private DcMotorEx linearSlide, lazySusan;
-
-    int ctr = 1;
 
     static final double FEET_PER_METER = 3.28084;
 
-    // Lens intrinsics
-    // UNITS ARE PIXELS
-    // NOTE: this calibration is for the C920 webcam at 800x448.
-    // You will need to do your own calibration for other configurations!
     double fx = 578.272;
     double fy = 578.272;
     double cx = 402.145;
     double cy = 221.506;
 
-    // UNITS ARE METERS
     double tagsize = 0.166;
 
-    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
-
+    int ID_TAG_OF_INTEREST = 18;
     int TAG2 = 2;
     int TAG3 = 10;
 
     public int NumberOfTag = 0;
 
-    AprilTagDetection tagOfInterest = null;
-
     int conesUp = 0;
 
+    //
+    AprilTagDetection tagOfInterest = null;
     Trajectory forwards = null;
     Trajectory left = null;
     Trajectory right = null;
@@ -69,18 +61,21 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
     Trajectory parkPosition2 = null;
     Trajectory parkPosition3 = null;
     Trajectory returnus = null;
+    private boolean slide = false;
 
-    private int bottomStop = 0;//bottom, stop here
+    // LinearSlide positions
+    private int bottomStop = 0;
     private int lowStop = 1150;
     private int midStop = 1950;
-    private int tallStop= 2700;//placeholder value because slide isn't currently tall enough to reach the "tallStop"
-    private int tooTall = 2970;//max height
-    private int target =     0;//placeholder here, gets used in function LinearSlideToStop()
-    private boolean slide = false;
-    private int hopStop = 270;
-    public int AutoMove = 460;
-    public int coneStack = 360;
-    public int insert  = 2500;
+    private int tallStop= 2700;
+    private int target =     0;
+    private int hopStop =  270;
+    private int AutoMove = 460;
+    private int coneStack= 360;
+    private int insert  = 2500;
+
+
+
 
     @Override
     public boolean LinearSlideToStop2(int stop, int tolerance, int conesUp){
@@ -127,117 +122,6 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
             return false;
     }
 
-
-    @Override
-    public void LeftAndDump(){
-        drive.followTrajectory(left); //TODO no comments!!!
-        //drive.moveTestServo(1);
-        //drive.LinearSlideToStop2(0,35, conesUp);
-        //drive.susanToEncoderPosition(0);
-    }
-
-    @Override
-    public void RightAndPickup(){
-        drive.followTrajectory(right);
-        //drive.moveTestServo(.5);
-        //drive.LinearSlideToStop2(3,35,conesUp);
-        //drive.susanToEncoderPosition(60);
-        //conesUp++;
-    }
-
-    @Override
-    public void CenterOnTile(Pose2d position){
-
-        double targetX = 0;
-        double targetY = 0;
-        double points[][] = {};
-        double centerX1 = 12 - position.getX();
-        double centerX2 = 36 - position.getX();
-        double centerX3 = 60 - position.getX();
-        double centerX4 = -12 - position.getX();
-        double centerX5 = -36 - position.getX();
-        double centerX6 = -60 - position.getX();
-
-        double centerY1 = 12 - position.getY();
-        double centerY2 = 36 - position.getY();
-        double centerY3 = 60 - position.getY();
-        double centerY4 = -12 - position.getY();
-        double centerY5 = -36 - position.getY();
-        double centerY6 = -60 - position.getY();
-
-        if(Math.abs(centerX1)<=12)
-            targetX = centerX1;
-        else if(Math.abs(centerX2)<=12)
-            targetX = centerX2;
-        else if(Math.abs(centerX3)<=12)
-            targetX = centerX3;
-        else if(Math.abs(centerX4)<=12)
-            targetX = centerX4;
-        else if(Math.abs(centerX5)<=12)
-            targetX = centerX5;
-        else if(Math.abs(centerX6)<=12)
-            targetX = centerX6;
-
-        if(Math.abs(centerY1)<=12)
-            targetY = centerY1;
-        else if(Math.abs(centerY2)<=12)
-            targetY = centerY2;
-        else if(Math.abs(centerY3)<=12)
-            targetY = centerY3;
-        else if(Math.abs(centerY4)<=12)
-            targetY = centerY4;
-        else if(Math.abs(centerY5)<=12)
-            targetY = centerY5;
-        else if(Math.abs(centerY6)<=12)
-            targetY = centerY6;
-
-
-
-        Trajectory Center = drive.trajectoryBuilder(position)
-                .lineTo(new Vector2d(targetX,targetY))
-                .build();
-
-        drive.followTrajectory(Center);
-
-    }
-
-    @Override
-    public void Park(int location){
-
-        drive.LinearSlideResetEnc();
-
-        Pose2d destination = null;
-
-
-        if(location == 1)
-            destination = new Pose2d(12,-36);
-        else if(location == 2)
-            destination = new Pose2d(36,-36);
-        else if(location == 3)
-            destination = new Pose2d(60 ,-36);
-        else if(true)
-            destination = new Pose2d(36,-36);
-
-
-        Vector2d x = new Vector2d(destination.getX() , drive.getPoseEstimate().getY()+.001);
-        Vector2d y = new Vector2d(drive.getPoseEstimate().getX()+.001 , destination.getY());
-
-
-
-        Trajectory parkX = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .lineTo(x)
-                .build();
-
-        Trajectory parkY = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .lineTo(y)
-                .build();
-
-
-        drive.followTrajectory(parkY);
-        drive.followTrajectory(parkX);
-    }
-
-
     @Override
     public void FirstCone() throws InterruptedException {
 
@@ -275,23 +159,13 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
         drive.LinearSlideToStop2(9,30,0);
         Thread.sleep(250);
         drive.moveTestServo(.25);
-        /* Thread.sleep(750);
+        Thread.sleep(750);
         if(NumberOfTag == 1)
             drive.followTrajectory(parkPosition1);
         else if(NumberOfTag == 2)
             drive.followTrajectory(parkPosition2);
         else if(NumberOfTag == 3)
-            drive.followTrajectory(parkPosition3); */
-
-
-
-
-
-        //drive.susanToEncoderPosition(2000);
-        //Thread.sleep(x);
-        //drive.moveTestServo(1);
-        //drive.susanToEncoderPosition(0);
-        //drive.LinearSlideToStop2(0,35,0);
+            drive.followTrajectory(parkPosition3);
     }
 
 
@@ -303,7 +177,6 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
 
         drive.ResetSusan();
 
-        // Declare the used non-drive motors.
 
         lazySusan = hardwareMap.get(DcMotorEx.class,"lazySusan");
 
@@ -311,29 +184,18 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
 
         clawServo = hardwareMap.get(Servo.class, "clawServo");
 
-
-        double speed = .5; //Speed multiplier for the drivetrain.
-
-        boolean slideY = false; //state of movement (yes or no) for different target slide positions.
-        boolean slideX = false;
-        boolean slideA = false;
-        boolean slideB = false;
-
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap); //Instance of SampleMecanum drive to access methods in the file.
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //Start encoders at position 0.
         lazySusan.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         lazySusan.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //Sets HALO to brake mode.
-        lazySusan.setDirection(DcMotorSimple.Direction.REVERSE);
 
         forwards = drive.trajectoryBuilder(new Pose2d())
 
-                .lineTo( new Vector2d(55,0)/*,
-                        drive.getVelocityConstraint(15, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
-                        drive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)*/
-                )
+                .lineTo( new Vector2d(55,0))
                 .build();
+
         returnus = drive.trajectoryBuilder(forwards.end())
                 .lineTo(new Vector2d(50,0))
                 .build();
@@ -351,29 +213,32 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
                         drive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
                 )
                 .build();
+
         toCones = drive.trajectoryBuilder(left.end())
                 .lineTo( new Vector2d(46.8,25.2),
                     drive.getVelocityConstraint(24, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
                     drive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
                 )
                 .build();
+
         toPole = drive.trajectoryBuilder(toCones.end())
                 .lineTo(new Vector2d(51,-10.4),
                         drive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
                         drive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
                 )
                 .build();
+
         parkPosition1 = drive.trajectoryBuilder(toPole.end())
                 .lineTo(new Vector2d(48,24))
                 .build();
+
         parkPosition3 = drive.trajectoryBuilder(toPole.end())
                 .lineTo(new Vector2d(48,0))
                 .build();
+
         parkPosition2 = drive.trajectoryBuilder(toPole.end())
                 .lineTo(new Vector2d(48,-24))
                 .build();
-
-
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
@@ -397,15 +262,11 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
 
         telemetry.setMsTransmissionInterval(50);
 
-        /*
-         * The INIT-loop:
-         * This REPLACES waitForStart!
-         */
+                //Init loop!
+
         while (!isStarted() && !isStopRequested())
         {
             ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
-
-            //camera.startStreaming(800,448); //TODO does this work?
 
             if(currentDetections.size() != 0)
             {
@@ -417,33 +278,20 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
                     {
                         tagOfInterest = tag;
                         tagFound = true;
-
                         NumberOfTag = 1;
-
                         break;
-
-
-
                     }
                     if(tag.id == TAG2){
                         tagOfInterest = tag;
                         tagFound = true;
-
                         NumberOfTag = 2;
-
                         break;
-
-
                     }
                     if(tag.id == TAG3){
                         tagOfInterest = tag;
                         tagFound = true;
-
                         NumberOfTag = 3;
-
                         break;
-
-
                     }
                 }
 
@@ -466,7 +314,6 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
                         tagToTelemetry(tagOfInterest);
                     }
                 }
-
             }
             else
             {
@@ -481,19 +328,14 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
                     telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                     tagToTelemetry(tagOfInterest);
                 }
-
             }
 
             telemetry.update();
             sleep(20);
         }
 
-        /*
-         * The START command just came in: now work off the latest snapshot acquired
-         * during the init loop.
-         */
+            //start here!!
 
-        /* Update the telemetry */
         if(tagOfInterest != null)
         {
             telemetry.addLine("Tag snapshot:\n");
@@ -506,44 +348,7 @@ public class AUTO extends LinearOpMode implements AUTOinterface {
             telemetry.update();
         }
 
-        /* Actually do something useful */
-        if(tagOfInterest == null)
-        {
-            FirstCone();
-           /* drive.turn(Math.toRadians(90));
-            Thread.sleep(50);
-            LeftAndDump();
-            Thread.sleep(50);
-            RightAndPickup();
-            Thread.sleep(50);
-            LeftAndDump();
-            Thread.sleep(50);
-            RightAndPickup();
-            Thread.sleep(50);
-            LeftAndDump();
-            Thread.sleep(50);
-            RightAndPickup();
-            Thread.sleep(50);
-            LeftAndDump();*/
-            //Park(2);
-        }
-        else {
-            FirstCone();
-            LeftAndDump();
-            RightAndPickup();
-            LeftAndDump();
-            RightAndPickup();
-            LeftAndDump();
-            RightAndPickup();
-            LeftAndDump();
-            //Park(NumberOfTag);
-
-
-        }
-
-
-        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
-        //while (opModeIsActive()) {sleep(20);}
+        FirstCone();
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AUTOinterface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AUTOinterface.java
@@ -4,14 +4,5 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 public interface AUTOinterface {
     boolean LinearSlideToStop2(int stop, int tolerance, int conesUp);
-
-    void LeftAndDump();
-
-    void RightAndPickup();
-
-    void CenterOnTile(Pose2d position);
-
-    void Park(int location);
-
     void FirstCone() throws InterruptedException;
 }
