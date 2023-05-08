8d30afd59102145d677d45585b76556b3b854724
addiment <63932310+addiment@users.noreply.github.com>
2023-05-08 13:14:37 -0400

    rework eventd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventd/EventEmitter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventd/EventEmitter.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.eventd;
 
 import android.util.Log;
+import androidx.annotation.NonNull;
 import java.util.ArrayList;
+
 public final class EventEmitter<T> {
     @FunctionalInterface
     public interface Listener<T> {
@@ -10,15 +12,23 @@ public final class EventEmitter<T> {
     }
 
     public void on(Listener<T> f) {
-        _listeners.add(f);
+        listeners.add(f);
     }
-    public void off(Listener<T> f) { _listeners.remove(f); }
+    public void off(Listener<T> f) { listeners.remove(f); }
 
     public void emit(T data) {
         Log.d("eventd", "Emit event, remove me later");
-        for (Listener<T> listener : _listeners) listener.run(data);
+        for (Listener<T> listener : listeners) listener.run(data);
+        if (baseListeners != null) for (Listener<T> listener : baseListeners) listener.run(data);
     }
 
-    private final ArrayList<Listener<T>> _listeners = new ArrayList<>();
-    public EventEmitter() { }
+    private final ArrayList<Listener<T>> listeners = new ArrayList<>();
+    private final Listener<T>[] baseListeners;
+    public EventEmitter() { baseListeners = null; }
+    @SafeVarargs
+    public EventEmitter(@NonNull Listener<T>... bases) throws ClassCastException {
+        for (Listener<T> b : bases) //noinspection ConstantConditions
+            if (!(b instanceof Listener)) throw new ClassCastException();
+        baseListeners = bases;
+    }
 }
