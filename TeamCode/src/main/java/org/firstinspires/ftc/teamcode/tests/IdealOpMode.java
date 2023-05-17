1a7819bda59bc9d001f8f08f4eb15a883a3bddb6
addiment <63932310+addiment@users.noreply.github.com>
2023-05-17 13:22:06 -0400

    works...?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/IdealOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/IdealOpMode.java
@@ -12,7 +12,7 @@ import java.util.EnumMap;
 import kotlin.Triple;
 
 @TeleOp(name = "Ideal OpMode")
-public class IdealOpMode extends OpMode {
+public class IdealOpMode extends StatefulOpMode {
 
     SoundPlayer soundPlayer = null;
     Thread phWorker;
@@ -31,13 +31,16 @@ public class IdealOpMode extends OpMode {
         DEBUG
     }
 
-    private static class phWorkerRoutine implements Runnable {
-        private final OpMode opMode;
+    private static final class phWorker implements Runnable {
+        private final StatefulOpMode opMode;
 
         private final EnumMap<Button, DigitalAction> mappings = new EnumMap<>(Button.class);
+        private final EnumMap<DigitalAction, Boolean> digitalState = new EnumMap<>(DigitalAction.class);
         private final EnumMap<DigitalAction, DigitalActionCallback> digitalRoutines = new EnumMap<>(DigitalAction.class);
 
-        phWorkerRoutine(OpMode opMode) { this.opMode = opMode; }
+        phWorker(StatefulOpMode opMode) {
+            this.opMode = opMode;
+        }
 
         // overload for analog
 
@@ -45,8 +48,18 @@ public class IdealOpMode extends OpMode {
 
         public void bindAction(DigitalAction action, DigitalActionCallback routine) { digitalRoutines.put(action, routine); }
 
+        public boolean getActionValue(DigitalAction action) {
+            return Boolean.TRUE.equals(digitalState.get(action));
+        }
+
         @Override
         public void run() {
+//            try {
+//                this.wait();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
             boolean stateA = opMode.gamepad1.a;
             boolean stateB = opMode.gamepad1.b;
 
@@ -73,9 +86,16 @@ public class IdealOpMode extends OpMode {
                     Triple<Button, Boolean, Boolean> triple = (Triple<Button, Boolean, Boolean>) e;
 
                     if (triple.component2() != triple.component3()) {
+                        opMode.telemetry.addLine("state change");
+                        opMode.telemetry.update();
+                        // get action bound to input
                         DigitalAction a = mappings.get(triple.component1());
                         if (a != null) {
+                            // update state
+                            digitalState.put(a, triple.component2());
+                            // get callback
                             DigitalActionCallback r = digitalRoutines.get(a);
+                            // run if valid
                             if (r != null) r.run(triple.component2());
                         }
                     }
@@ -90,14 +110,18 @@ public class IdealOpMode extends OpMode {
         }
     }
 
-    private static void phWorkerExceptionHandler(Thread t, Throwable e) { throw new RuntimeException(e); }
+    private static void phWorkerExceptionHandler(Thread t, Throwable e) {
+//        if (e instanceof InterruptedException) return;
+        throw new RuntimeException(e);
+    }
 
     @Override
     public void init() {
+        this.hasInit = true;
         soundPlayer = SoundPlayer.getInstance();
         Gamepadyn.opmodeInit(this);
 
-        phWorkerRoutine routine = new phWorkerRoutine(this);
+        IdealOpMode.phWorker routine = new phWorker(this);
         phWorker = new Thread(routine);
         phWorker.setUncaughtExceptionHandler(IdealOpMode::phWorkerExceptionHandler);
 
@@ -105,6 +129,8 @@ public class IdealOpMode extends OpMode {
         routine.bindInput(Button.BUTTON_A, DigitalAction.DEBUG);
 
         routine.bindAction(DigitalAction.DEBUG, (value) -> {
+            telemetry.addLine("event DEBUG " + value);
+            telemetry.update();
             soundPlayer.startPlaying(
                 hardwareMap.appContext,
                 (value ? R.raw.debug_press : R.raw.debug_release)/*,
@@ -117,11 +143,18 @@ public class IdealOpMode extends OpMode {
 
     @Override
     public void start() {
+        this.isStarted = true;
+        phWorker.start();
+//        phWorker.notify();
         Gamepadyn.opmodeStart(this);
     }
 
     @Override
     public void stop() {
+        this.isStarted = false;
+        this.hasInit = false;
+        phWorker.interrupt();
+        phWorker = null;
         Gamepadyn.opmodeStop();
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/StatefulOpMode.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class StatefulOpMode extends OpMode {
+    public boolean isStarted = false;
+    public boolean hasInit = false;
+}
